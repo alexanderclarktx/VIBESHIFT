8c8c01c3e22a9c96980409a1cbdba9b31a424578
Alex <petrovich.alex96@gmail.com>
2025-09-06 16:53:09 -0700

    wip

--- src/AI.ts
+++ src/AI.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai"
 
-const apiKey = (b64: string) => `sk-admin-${atob(b64)}`
+const apiKey = (b64: string) => `sk-proj-${atob(b64)}`
 
 type AI = {
   prompt: (prompt: string, callback: (response: string) => void) => Promise<void>
@@ -9,19 +9,23 @@ type AI = {
 export const AI = (): AI => {
   const client = new OpenAI({
     dangerouslyAllowBrowser: true,
-    apiKey: apiKey("SkNRZnhVLUtQWXU4WExVNkREMGkydkJhdHFTOC1hZTRGQW1NSGhYVW1JbWdkZVptcGwwcmpiMFFNTVQzQmxia0ZKelhLZGtISHE2ZldrdEdmRDh1T3BFOUxiQ2RySGdHVS1NNUt5U0lIREFfa19ZNUJiNUNMakpRTnBvQQ==")
+    apiKey: apiKey("UlktWU9id0F0eldiczBsRy1GRm8xeDZjaGlrWUhhSXNIZUFIYzgySHVuOEFELU5ndXQtdWYyeF91aVNTZkp6YmFLamM2YVlValhUM0JsYmtGSkwxZUV6dEpMQkZEMVU2TXhtNDkzdkstUUwwSkxkZHZWR040VkhaN1EyaUY0Y3MwdlJBZFYzdzlkNlFZaVB6d0pybzBNQ0hDLVVB")
   })
 
   return {
     prompt: async (prompt: string, callback) => {
-      const completion = await client.chat.completions.create({
-        model: "gpt-5-mini",
-        messages: [
-          { content: prompt, role: "user" }
-        ]
-      })
-
-      if (completion?.choices[0].message.content) callback(completion.choices[0].message.content)
+      try {
+        const completion = await client.chat.completions.create({
+          model: "gpt-5-mini",
+          messages: [
+            { content: prompt, role: "user" }
+          ]
+        })
+        if (completion?.choices[0].message.content) callback(completion.choices[0].message.content)
+      } catch (e) {
+        throw e
+        console.warn("error prompting", e)
+      }
     }
   }
 }
