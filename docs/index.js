e666db30d20f7996d078fb574fa3a07336f73b59
Alex <petrovich.alex96@gmail.com>
2025-09-06 16:28:11 -0700

    wip

--- docs/index.js
+++ docs/index.js
@@ -5594,7 +5594,7 @@ var _deployments_endpoints = new Set([
 var AI = () => {
   const client = new OpenAI({
     dangerouslyAllowBrowser: true,
-    apiKey: "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"
+    apiKey: ""
   });
   return {
     prompt: async (prompt, callback) => {
