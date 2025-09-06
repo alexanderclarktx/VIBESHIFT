30ea9beddfea6471df5cb6f34f64c853a7e6abe8
Alex <petrovich.alex96@gmail.com>
2025-09-06 16:35:45 -0700

    wip

--- src/AI.ts
+++ src/AI.ts
@@ -1,5 +1,7 @@
 import OpenAI from "openai"
 
+const apiKey = "sk-" + "proj-" + ""
+
 type AI = {
   prompt: (prompt: string, callback: (response: string) => void) => Promise<void>
 }
@@ -7,7 +9,7 @@ type AI = {
 export const AI = (): AI => {
   const client = new OpenAI({
     dangerouslyAllowBrowser: true,
-    apiKey: "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"
+    apiKey
   })
 
   return {
